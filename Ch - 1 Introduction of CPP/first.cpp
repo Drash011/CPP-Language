80c4e32b2ee6577f0bf413f2d398344858532652
TDrashti01 <thummard097@gmail.com>
2026-04-25 16:22:42 +0530

    C++ Language

---
+++ Ch - 1 Introduction of CPP/first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello I'm Drashti Thummar" << endl;
+    cout << "I'm Frontend Developer" << endl;
+
+    return 0;
+}
---
+++ Ch - 1 Introduction of CPP/rectangular_box.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    cout << "----------------" << endl;
+    cout << "|              |" << endl;
+    cout << "|  5 x 5 = 25  |" << endl;
+    cout << "|              |" << endl;
+    cout << "----------------" << endl;
+
+    return 0;
+}
