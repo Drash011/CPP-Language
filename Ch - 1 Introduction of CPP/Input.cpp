28e9fa41239210078b1cb4e9f4a7737ee5f69e09
TDrashti01 <thummard097@gmail.com>
2026-04-27 16:02:16 +0530

    Chapter 1

---
+++ Ch - 1 Introduction of CPP/Input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+
+    cout << "Enter First Number : ";
+    cin >> a;
+    
+    cout << "Enter Second Number : ";
+    cin >> b;
+
+    cout << endl;
+
+    cout << a << "\t" << b << "\t" << endl;
+
+    cout << endl;
+
+    cout << a << " + " << b << " = " << a + b << endl;  
+
+    return 0;
+}
