8952c5bc78328412d0b4e0c0a30aa4a83a206131
TDrashti01 <thummard097@gmail.com>
2026-04-28 15:37:06 +0530

    Chapter 2

---
+++ Ch - 2 Programming with OOPs/Lab Work 2-1/q-2-Car_Record_System_Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "q-2-Car_Record_System.cpp"
+
+int main()
+{
+
+    Car_Record c1, c2, c3, c4;
+
+    c1.setCarRecordSystem();
+    c2.setCarRecordSystem();
+    c3.setCarRecordSystem();
+    c4.setCarRecordSystem();
+
+    c1.getCarRecordSystem();
+    c2.getCarRecordSystem();
+    c3.getCarRecordSystem();
+    c4.getCarRecordSystem();
+
+    return 0;
+}
