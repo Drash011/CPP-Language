ba5f8f3b6b48de191fcff4c2470f8560c8bfd6e9
TDrashti01 <thummard097@gmail.com>
2026-05-04 16:43:54 +0530

    Chapter 3

---
+++ Ch - 3 Data Encapsulation/Lab Work 3-1/q-2-Customer_Record_Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "q-2-Customer_Record.cpp"
+
+int main ()
+{
+    CustomarRecord c1, c2, c3, c4, c5;
+
+    c1.setCustomarRecord();
+    c2.setCustomarRecord();
+    c3.setCustomarRecord();
+    c4.setCustomarRecord();
+    c5.setCustomarRecord();
+
+    c1.getCustomarRecord();
+    c2.getCustomarRecord();
+    c3.getCustomarRecord();
+    c4.getCustomarRecord();
+    c5.getCustomarRecord();
+
+    return 0;
+}
